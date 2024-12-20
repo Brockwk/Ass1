1077c15bef47a969b9c8b5c81c56cc8eedc48fda
Rani Dhage <ranidhage@Ranis-MacBook-Air.local>
2024-12-20 15:58:46 +0530

    data cleaning code

---
+++ demo.py
@@ -0,0 +1,32 @@
+from google.cloud import storage
+import pandas as pd
+from io import StringIO
+
+# Initialize the Cloud Storage client
+storage_client = storage.Client()
+
+# Specify the bucket name and file name
+bucket_name = 'zago1'
+source_blob_name = 'data.csv'  # Original file in the bucket
+
+# Get the bucket object
+bucket = storage_client.get_bucket(bucket_name)
+
+# Get the blob (file) object
+blob = bucket.blob(source_blob_name)
+
+# Download the content of the file as a string
+data = blob.download_as_text()
+
+# Read the CSV data into a pandas DataFrame
+df = pd.read_csv(StringIO(data))
+
+# Remove duplicates
+df.drop_duplicates(inplace=True)
+
+# Fill empty or NaN values with 'null'
+df.fillna('null', inplace=True)
+
+# Print the cleaned data
+print("Cleaned DataFrame:")
+print(df)
